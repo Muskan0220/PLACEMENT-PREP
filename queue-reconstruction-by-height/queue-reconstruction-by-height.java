68b564483dbc24737012f4da848a3e5f521fe18f
MUSKAN AGRAWAL <63292583+Muskan0220@users.noreply.github.com>
2021-05-17 23:41:41 +0530

    Time: 5 ms (99.38%), Space: 39.8 MB (56.92%) - LeetHub

---
+++ queue-reconstruction-by-height/queue-reconstruction-by-height.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+         Arrays.sort(people, (c1, c2) -> {
+           if (c1[0] == c2[0]) {
+               return c1[1] - c2[1];
+           } else {
+               return c2[0] - c1[0];
+           }
+        });
+        
+        List<int[]> res = new ArrayList<>();
+        for (int[] person : people) {
+            res.add(person[1], person);
+        }
+        return res.toArray(new int[1][1]);
+        
+    }
+}
