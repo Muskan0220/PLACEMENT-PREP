34b41274b505c0835f3cbab47927dd0356c0142b
MUSKAN AGRAWAL <63292583+Muskan0220@users.noreply.github.com>
2021-06-20 04:40:49 +0530

    Time: 0 ms (100.00%), Space: 37.8 MB (51.90%) - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle==null){
+            return 0;
+        }
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
+        }else{
+            return -1;
+        }
+    }
+}
